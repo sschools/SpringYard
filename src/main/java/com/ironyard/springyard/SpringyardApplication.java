65a021588b081891014ce1e73237f8594f433ebd
Stephen Schools <stephen.schools>
2017-10-04 16:45:55 -0400

    initial commit

---
+++ src/main/java/com/ironyard/springyard/SpringyardApplication.java
@@ -0,0 +1,12 @@
+package com.ironyard.springyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringyardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringyardApplication.class, args);
+	}
+}
