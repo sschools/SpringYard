932ec44525cf7b9757eb6239a153d47c24478829
Stephen Schools <stephen.schools>
2017-10-04 17:03:08 -0400

    repository next

---
+++ src/main/java/com/ironyard/springyard/model/Customer.java
@@ -0,0 +1,53 @@
+package com.ironyard.springyard.model;
+
+public class Customer {
+    int id;
+    String firstName;
+    String lastName;
+    String phone;
+    String email;
+
+    public Customer() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
