d75c516d41407bd956552d0f8160b5756e068bd0
Stephen Schools <stephen.schools>
2017-10-05 13:49:14 -0400

    tests working

--- src/test/java/com/ironyard/springyard/service/CustomerServiceTest.java
+++ src/test/java/com/ironyard/springyard/service/CustomerServiceTest.java
@@ -50,4 +50,85 @@ public class CustomerServiceTest {
         Assert.assertEquals(firstName, cust3.getFirstName());
         Assert.assertEquals(lastName, cust3.getLastName());
     }
+
+    @Test
+    public void testUpdate() {
+        String firstName = Long.toString(System.currentTimeMillis());
+        String lastName = Long.toString(System.currentTimeMillis());
+        String phone = Long.toString(System.currentTimeMillis());
+        String email = Long.toString(System.currentTimeMillis());
+
+        Customer cust1 = new Customer();
+        cust1.setFirstName(firstName);
+        cust1.setLastName(lastName);
+        cust1.setPhone(phone);
+        cust1.setEmail(email);
+        customerRepository.add(cust1);
+
+        List<Customer> customers = customerRepository.get();
+
+        Customer cust2 = null;
+        for (Customer c : customers) {
+            if (c.getFirstName().equals(firstName) && c.getLastName().equals(lastName)) {
+                cust2 = c;
+            }
+        }
+        Assert.assertNotNull(cust2);
+
+        String updateFirstName = Long.toString(System.currentTimeMillis());
+        String updateLastName = Long.toString(System.currentTimeMillis());
+
+        cust2.setFirstName(updateFirstName);
+        cust2.setLastName(updateLastName);
+        customerRepository.update(cust2);
+
+        customers = customerRepository.get();
+
+        Customer cust3 = null;
+        for (Customer c : customers) {
+            if (c.getFirstName().equals(updateFirstName) && c.getLastName().equals(updateLastName)) {
+                cust3 = c;
+            }
+        }
+        Assert.assertNotNull(cust3);
+
+        Assert.assertEquals(cust2.getId(), cust3.getId());
+    }
+
+    @Test
+    public void testDelete() {
+        String firstName = Long.toString(System.currentTimeMillis());
+        String lastName = Long.toString(System.currentTimeMillis());
+        String phone = Long.toString(System.currentTimeMillis());
+        String email = Long.toString(System.currentTimeMillis());
+
+        Customer cust1 = new Customer();
+        cust1.setFirstName(firstName);
+        cust1.setLastName(lastName);
+        cust1.setPhone(phone);
+        cust1.setEmail(email);
+        customerRepository.add(cust1);
+
+        List<Customer> customers = customerRepository.get();
+
+        Customer cust2 = null;
+        for (Customer c : customers) {
+            if (c.getFirstName().equals(cust1.getFirstName()) && c.getLastName().equals(cust1.getLastName())) {
+                cust2 = c;
+            }
+        }
+        Assert.assertNotNull(cust2);
+
+        customerRepository.delete(cust2.getId());
+
+        customers = customerRepository.get();
+
+        Customer cust3 = null;
+        for (Customer c : customers) {
+            if (c.getFirstName().equals(cust1.getFirstName()) && c.getLastName().equals(cust1.getLastName())) {
+                cust3 = c;
+            }
+        }
+        Assert.assertNull(cust3);
+    }
 }
