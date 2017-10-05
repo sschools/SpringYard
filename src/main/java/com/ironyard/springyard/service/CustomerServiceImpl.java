164f472f5b6189a2d473f7a660a13006a084841e
Stephen Schools <stephen.schools>
2017-10-05 13:06:08 -0400

    service impl set, write tests

--- src/main/java/com/ironyard/springyard/service/CustomerServiceImpl.java
+++ src/main/java/com/ironyard/springyard/service/CustomerServiceImpl.java
@@ -1,36 +1,44 @@
 package com.ironyard.springyard.service;
 
 import com.ironyard.springyard.model.Customer;
+import com.ironyard.springyard.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
-    @Autowired
     
+    @Autowired
+    private CustomerRepository customerRepository;
 
+    @Transactional
     @Override
     public void add(Customer customer) {
-
+        customerRepository.add(customer);
     }
 
+    @Transactional
     @Override
     public void update(Customer customer) {
-
+        customerRepository.update(customer);
     }
 
     @Override
     public Customer getById(int id) {
-        return null;
+        return customerRepository.getById(id);
     }
 
     @Override
     public List<Customer> get() {
-        return null;
+        return customerRepository.get();
     }
 
+    @Transactional
     @Override
     public void delete(int id) {
-
+        customerRepository.delete(id);
     }
 }
