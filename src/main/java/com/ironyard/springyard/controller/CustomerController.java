dae6ab335969d484b095805236f6ef73421cb489
Stephen Schools <stephen.schools>
2017-10-06 13:48:31 -0400

    basic functionality, need to style and want to add delete method

--- src/main/java/com/ironyard/springyard/controller/CustomerController.java
+++ src/main/java/com/ironyard/springyard/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.ironyard.springyard.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,13 @@ public class CustomerController {
     @RequestMapping(path = "/add-customer", method = RequestMethod.GET)
     public String addCustomer () {
         return "add-customer";
+    }
+
+    @RequestMapping(path = "/view-one/{id}", method = RequestMethod.GET)
+    public String viewOne (@PathVariable("id") int id, Model model) {
+        Customer customer = customerService.getById(id);
+        model.addAttribute("customer", customer);
+        return "view-one";
     }
 
     @RequestMapping(path = "/add-customer", method = RequestMethod.POST)
